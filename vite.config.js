efa4554e6794e459a1f3fdd3c38b6c9fc186b410
Hubert Krzysztofiak <hubertkrzyszt@gmail.com>
2024-10-14 17:47:05 +0200

    Try implement in firebas

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import 'dotenv/config'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/WishBook/" : "/",
+  base: "/",
   plugins: [
     vue(),
     vueJsx()
